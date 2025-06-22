f0e68ce18b7fc99d3a29c828267669f5a3fbcf0b
Maria Eduarda <mariaedsouza.machado@gmail.com>
2025-06-22 15:36:52 -0300

    Segundo commit: Criação do JS na página de login

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('login-input');
+const button = document.querySelector ('.login-button');
+const form =  document.querySelector ('.login-form');
+
+function validateInput ({ target }){
+   if (target.value.length > 4){
+    button.removeAttribute('disabled');
+    return;
+   } else {
+     button.setAttribute('disabled', '');
+   }
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    localStorage.setItem('player', input.value); //salvar as info
+    window.location = 'pages/memorygame.html';
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
